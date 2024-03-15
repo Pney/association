e536e7f4518a9574d1acd7c78f2f81610c5a42a8
Pney <thiagofernandes.123@hotmail.com>
2024-03-15 19:20:03 -0300

    Initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,50 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
 
+Debt.destroy_all
+Person.destroy_all
 User.destroy_all
 
-User.create email: 'admin@admin.com', password: '111111'
+puts "Data/Hora Inicio: #{Time.now}"
 
-puts "Usuário criado:"
-puts "login admin@admin.com"
-puts "111111"
+50.times do
+    user = User.create!(
+        email: Faker::Internet.email,
+        password: Faker::Internet.password(min_length: 8, special_characters: true)
+    )
+    puts user.inspect
+end
+
+users_ids = User.all.ids
+
+100.times do
+    person = Person.create!(
+        name: Faker::Name.name,
+        phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+        national_id: CPF.generate,
+        active: Faker::Boolean.boolean,
+        user_id: users_ids.sample
+    )
+    puts person.inspect
+end
+
+persons_ids = Person.all.ids
+
+500.times do
+    debt = Debt.create!(
+        person_id: persons_ids.sample,
+        amount: Faker::Number.positive,
+        observation: Faker::Quote.famous_last_words
+    )
+
+    puts debt.inspect
+end
+
+puts "Data/Hora Fim: #{Time.now}"
+
+user_example = User.find(User.pluck(:id).sample)
+puts "Usuário Criado:"
+puts "login #{user_example.email}"
+# puts "senha #{user_example.password}"
