98d034da23f0f7a2bb34118d3e283d78e33fb2c8
Pney <thiagofernandes.123@hotmail.com>
2024-04-23 20:37:30 -0300

    Add pagamentos

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,61 @@
+class PaymentsController < ApplicationController
+  before_action :set_payment, only: %i[show edit update destroy]
+
+  def index
+    @payments = Payment.paginate(page: params[:page]).order(id: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @payment = Payment.new
+  end
+
+  def edit
+  end
+
+  def create
+    @payment = Payment.new(payment_params)
+
+    respond_to do |format|
+      if @payment.save
+        format.html { redirect_to payment_url(@payment), notice: "Payment was successfully created." }
+        format.json { render :show, status: :created, location: @payment }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @payment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @payment.update(payment_params)
+        format.html { redirect_to payment_url(@payment), notice: "Payment was successfully updated." }
+        format.json { render :show, status: :ok, location: @payment }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @payment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @payment.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to payments_url, notice: "Payment was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_payment
+      @payment = Payment.find(params[:id])
+    end
+
+    def payment_params
+      params.require(:payment).permit(:person_id, :amount, :paid_at)
+    end
+end
